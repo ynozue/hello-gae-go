88f1dec30097adf7ad65b98214d778bd273095a7
yasu <yasu@MacBook-Pro.local>
2017-09-29 18:57:57 +0900

    first commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/hello", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Google App Engine!!")
+}
