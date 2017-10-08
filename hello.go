0696aabecce3d83ecdbc0afd4f6a23e63157f442
yasu <yasu@MacBook-Pro.local>
2017-10-08 20:48:34 +0900

    hello GAE

--- hello.go
+++ hello.go
@@ -1,14 +1,34 @@
-package hello
+package main
 
 import (
-	"fmt"
 	"net/http"
+	"encoding/json"
 )
+
+type Result struct {
+	Status int
+	Description string
+}
 
 func init() {
 	http.HandleFunc("/hello", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello Google App Engine!!")
+	var result Result
+	switch r.Method {
+	case http.MethodGet:
+		result = Result{http.StatusOK, "http get result"}
+	case http.MethodPost:
+		result = Result{http.StatusOK, "http post result"}
+	default:
+		result = Result{http.StatusNotImplemented, "not implemented http mehtod"}
+	}
+	res, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+	}
 }
